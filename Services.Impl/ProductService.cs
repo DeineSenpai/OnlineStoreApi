a61cdc9c442171df20505241be5538cdc8f63ac0
КондратьеваМ <@610-04>
2022-07-14 21:13:29 +0600

    Add project files.

---
+++ Services.Impl/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using OnlineStore.Model;
+using Services.Interfaces;
+namespace Services.Impl
+{
+    public class ProductService : IProductInterface
+    {
+        public async Task<Product>GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "product 1",
+                Price = 100,
+                Description = "new laptop with best price"
+            };
+        }
+
+        public async Task<List<Product>> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+            {
+                Id = 1,
+                Name = "product 1",
+                Price = 100,
+                Description = "new laptop with best price"
+            },
+                new Product()
+            {
+                Id = 1,
+                Name = "product 2",
+                Price = 200,
+                Description = "new laptop2 with best price2"
+            }
+            };
+        }
+    }
+}
